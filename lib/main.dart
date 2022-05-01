4930f247aae0276c8ab4785b38eccb94ced7daa6
EDR3X <dhunganaannanta@gmail.com>
2022-05-01 17:58:06 +0545

    Todo Class initiation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_cubit/pages/todo_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,21 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Todo App",
       theme: ThemeData(primarySwatch: Colors.green),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("todo App"),
-      ),
-      body: const Text("hello"),
+      home: const TodoPage(),
     );
   }
 }
