75b27711dc5da447104208af039a55913128e0f0
EDR3X <dhunganaannanta@gmail.com>
2022-05-02 16:20:35 +0545

    BlocProvider implemented in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_cubit/cubits/cubits.dart';
 import 'package:todo_cubit/pages/todo_page.dart';
 
 void main() => runApp(const MyApp());
@@ -8,11 +10,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Todo App",
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: const TodoPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodoFilterCubit>(
+          create: (context) => TodoFilterCubit(),
+        ),
+        BlocProvider<TodoSearchCubit>(
+          create: (context) => TodoSearchCubit(),
+        ),
+        BlocProvider<TodoListCubit>(
+          create: (context) => TodoListCubit(),
+        ),
+        BlocProvider<ActiveTodoCounterCubit>(
+          create: (context) => ActiveTodoCounterCubit(
+            todoListCubit: BlocProvider.of<TodoListCubit>(context),
+          ),
+        ),
+        BlocProvider<FilteredTodosCubit>(
+          create: (context) => FilteredTodosCubit(
+              todoFilterCubit: BlocProvider.of<TodoFilterCubit>(context),
+              todoSearchCubit: BlocProvider.of<TodoSearchCubit>(context),
+              todoListCubit: BlocProvider.of<TodoListCubit>(context)),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Todo App",
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: const TodoPage(),
+      ),
     );
   }
 }
