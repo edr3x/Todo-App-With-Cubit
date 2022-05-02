808dc8dc1fc1824cdbc63daa29927262461436f5
EDR3X <dhunganaannanta@gmail.com>
2022-05-02 17:54:34 +0545

    homepage ui implementation

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -5,10 +5,43 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Todo App"),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+            child: Center(
+              child: Column(
+                children: const [
+                  TodoHeader(),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const <Widget>[
+        Text(
+          'Todo',
+          style: TextStyle(fontSize: 40),
+        ),
+        Text(
+          '0 items left',
+          style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+        )
+      ],
     );
   }
 }
