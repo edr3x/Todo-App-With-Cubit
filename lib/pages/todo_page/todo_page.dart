cf0a74a0b0ffd1e376cfd725c9eea7b1d1b51629
EDR3X <dhunganaannanta@gmail.com>
2022-05-04 18:36:43 +0545

    Search and filter Todo UI and logic completed

--- lib/pages/todo_page/todo_page.dart
+++ lib/pages/todo_page/todo_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_cubit/cubits/active_todo_counter/active_todo_counter_cubit.dart';
 import 'package:todo_cubit/pages/todo_page/create_todo.dart';
+import 'package:todo_cubit/pages/todo_page/search_and_filter_todo.dart';
 import 'package:todo_cubit/pages/todo_page/todo_header.dart';
 
 class TodoPage extends StatelessWidget {
@@ -20,6 +21,8 @@ class TodoPage extends StatelessWidget {
                 children: const [
                   TodoHeader(),
                   CreateTodo(),
+                  SizedBox(height: 10.0),
+                  SearchAndFilterTodo(),
                 ],
               ),
             ),
