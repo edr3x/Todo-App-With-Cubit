2662648a89b6c9fd59d076fa12c2badb8f6666d8
EDR3X <dhunganaannanta@gmail.com>
2022-05-08 00:11:33 +0545

    refactor

--- lib/pages/todo_page/show_todos.dart
+++ lib/pages/todo_page/show_todos.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:todo_cubit/cubits/cubits.dart';
 import 'package:todo_cubit/models/todo_model.dart';
 
--- lib/pages/todo_page/todo_page.dart
+++ lib/pages/todo_page/todo_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_cubit/cubits/active_todo_counter/active_todo_counter_cubit.dart';
 import 'package:todo_cubit/pages/todo_page/create_todo.dart';
 import 'package:todo_cubit/pages/todo_page/search_and_filter_todo.dart';
 import 'package:todo_cubit/pages/todo_page/show_todos.dart';
