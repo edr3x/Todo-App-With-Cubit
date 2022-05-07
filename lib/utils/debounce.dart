2857a3fbf7b3304669b4c58e84f8dc0bcd618b34
EDR3X <dhunganaannanta@gmail.com>
2022-05-07 23:44:49 +0545

    debounce init

---
+++ lib/utils/debounce.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class Debounce {
+  final int miliseconds;
+  Debounce({
+    this.miliseconds = 500,
+  });
+
+  Timer? _timer;
+
+  void run(VoidCallback action) {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+
+    _timer = Timer(Duration(milliseconds: miliseconds), action);
+  }
+}
